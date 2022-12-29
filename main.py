1df92d636a8f4f7562b4c954edb0d21d6d3f44c5
Hill-0xFF <leadlevelprogrammer@gmail.com>
2022-12-29 15:37:55 -0500

    [dev]: Created Ball class

--- main.py
+++ main.py
@@ -26,6 +26,14 @@ class GameObject(object):
     def delete(self):
         self.canvas.delete(self.item)
 
+class Ball(GameObject):
+    def __init__(self, canvas, pos_x, pos_y):
+        self.radius = 10
+        self.direction = [1, -1]
+        self.speed = 10
+        item = canvas.create_oval(pos_x - self.radius, pos_y - self.radius, pos_x + self.radius, pos_y + self.radius, fill = 'white' )
+        super(Ball, self).__init__(canvas, item)
+
 if __name__ == '__main__':
     main = tk.Tk()
     main.title('Breakout_Py')
@@ -33,5 +41,5 @@ if __name__ == '__main__':
 
     item = game.canvas.create_rectangle(10, 10, 100, 80, fill='green') # paddle
     game_object = GameObject(game.canvas, item)
-
+    
     game.mainloop()
